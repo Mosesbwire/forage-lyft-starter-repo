8b0441d850895811952ef2b4b05136891dff954c
Mosesbwire <mosesdev92@gmail.com>
2023-10-12 11:14:28 +0300

    add constructor to init Car object

--- car/car.py
+++ car/car.py
@@ -5,20 +5,10 @@ from interface.servicable import Servicable
 
 
 class Car(Servicable):
-    battery: AbstractBattery
-    engine: AbstractEngine
 
-    def set_engine(self, engine: AbstractEngine) -> None:
-        self.engine = engine
-
-    def set_battery(self, battery: AbstractBattery) -> None:
-        self.battery = battery
-
-    def get_battery(self):
-        return self.battery
-
-    def get_engine(self):
-        return self.engine
+    def __init__(self, engine: AbstractEngine, battery: AbstractBattery) -> None:
+        self.engine: AbstractEngine = engine
+        self.battery: AbstractBattery = battery
 
     def needs_service(self) -> bool:
         return self.battery.needs_service() or self.engine.needs_service()
