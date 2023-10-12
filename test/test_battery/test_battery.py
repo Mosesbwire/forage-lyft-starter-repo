dc63a4006a7c72ffcd2e593ae057f7e9bf5e8c05
Mosesbwire <mosesdev92@gmail.com>
2023-10-12 17:43:15 +0300

    Refactor code to use variable to represent years passed since last battery service. The refactor allows for easy change of unit test code if service threshold for battery is changed

--- test/test_battery/test_battery.py
+++ test/test_battery/test_battery.py
@@ -1,7 +1,8 @@
 
 from battery.nubbin_battery import NubbinBattery
 from battery.spindler_battery import SpindlerBattery
-from datetime import datetime
+from datetime import datetime as dt
+import datetime
 from dateutil import parser
 import unittest
 
@@ -9,16 +10,21 @@ import unittest
 class TestNubbin(unittest.TestCase):
 
     def test_battery_should_be_serviced(self):
-        last_service_date = parser.parse("January 1, 2016 8:00 AM")
-        current_date = datetime.now()
+
+        last_service = 4.5
+        current_date = dt.now()
+        last_service_date = current_date - \
+            datetime.timedelta(days=365 * last_service)
 
         battery = NubbinBattery(last_service_date, current_date)
 
         self.assertTrue(battery.needs_service())
 
     def test_battery_does_not_need_service(self):
-        last_service_date = parser.parse("December 25, 2020 8:00 PM")
-        current_date = datetime.now()
+        last_service = 3
+        current_date = dt.now()
+        last_service_date = current_date - \
+            datetime.timedelta(days=365 * last_service)
 
         battery = NubbinBattery(last_service_date, current_date)
 
@@ -28,16 +34,20 @@ class TestNubbin(unittest.TestCase):
 class TestSpindler(unittest.TestCase):
 
     def test_battery_should_be_serviced(self):
-        last_service_date = parser.parse("January 1, 2020 10:00 AM")
-        current_date = datetime.now()
+        last_service = 2.5
+        current_date = dt.now()
+        last_service_date = current_date - \
+            datetime.timedelta(days=365 * last_service)
 
         battery = SpindlerBattery(last_service_date, current_date)
 
         self.assertTrue(battery.needs_service())
 
     def test_battery_does_not_need_service(self):
-        last_service_date = parser.parse("December 25, 2022 1:00 PM")
-        current_date = datetime.now()
+        last_service = 1
+        current_date = dt.now()
+        last_service_date = current_date - \
+            datetime.timedelta(days=365 * last_service)
 
         battery = SpindlerBattery(last_service_date, current_date)
 
