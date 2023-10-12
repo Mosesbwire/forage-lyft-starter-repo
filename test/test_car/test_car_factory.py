9115d1c38fe1b9c75727ead677939b1e0071038b
Mosesbwire <mosesdev92@gmail.com>
2023-10-12 17:58:59 +0300

    Refactor test_car_factory.py unit test code. use variable to represent time elapsed since last service instead of hard coded string values representing time

--- test/test_car/test_car_factory.py
+++ test/test_car/test_car_factory.py
@@ -1,5 +1,6 @@
 
-from datetime import datetime
+from datetime import datetime as dt
+import datetime
 from dateutil import parser
 from car.car_factory import CarFactory
 import unittest
@@ -9,8 +10,10 @@ from unittest.mock import Mock
 class TestCarFactory(unittest.TestCase):
 
     def test_car_is_calliope(self):
-        current_date = datetime.now()
-        last_service_date = parser.parse("January 1, 2020 8:00 AM")
+        last_service = 3.5
+        current_date = dt.now()
+        last_service_date = current_date - \
+            datetime.timedelta(days=365 * last_service)
         current_mileage = 40000
         last_mileage = 0
         car = CarFactory.create_calliope(
@@ -19,8 +22,10 @@ class TestCarFactory(unittest.TestCase):
         self.assertTrue(car.needs_service())
 
     def test_car_is_glissade(self):
-        current_date = datetime.now()
-        last_service_date = parser.parse("January 1, 2020 8:00 AM")
+        last_service = 3.5
+        current_date = dt.now()
+        last_service_date = current_date - \
+            datetime.timedelta(days=365 * last_service)
         current_mileage = 60000
         last_mileage = 0
         car = CarFactory.create_calliope(
@@ -29,7 +34,7 @@ class TestCarFactory(unittest.TestCase):
         self.assertTrue(car.needs_service())
 
     def test_car_is_palindrome(self):
-        current_date = datetime.now()
+        current_date = dt.now()
         last_service_date = parser.parse("January 1, 2020 8:00 AM")
         warning_light = True
 
@@ -39,8 +44,10 @@ class TestCarFactory(unittest.TestCase):
         self.assertTrue(car.needs_service())
 
     def test_car_is_rorschach(self):
-        current_date = datetime.now()
-        last_service_date = parser.parse("January 1, 2017 8:00 AM")
+        last_service = 4.5
+        current_date = dt.now()
+        last_service_date = current_date - \
+            datetime.timedelta(days=365 * last_service)
         current_mileage = 60000
         last_mileage = 0
         car = CarFactory.create_rorschach(
@@ -49,8 +56,10 @@ class TestCarFactory(unittest.TestCase):
         self.assertTrue(car.needs_service())
 
     def test_car_is_thovex(self):
-        current_date = datetime.now()
-        last_service_date = parser.parse("January 1, 2017 8:00 AM")
+        last_service = 4.5
+        current_date = dt.now()
+        last_service_date = current_date - \
+            datetime.timedelta(days=365 * last_service)
         current_mileage = 35000
         last_mileage = 0
         car = CarFactory.create_rorschach(
